ee828c8632aef32d7feda952b23d64549864c51d
Mayank Rajput <103987586+mayanksinghrajput1212@users.noreply.github.com>
2024-06-27 16:00:17 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android_devlopment_quiz_app"
+include(":app")
+ 
